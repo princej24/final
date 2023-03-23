5e4c4a5568e49b385f70377cfc05896a02b89802
princej24 <pjmuturi@students.rtc.edu>
2023-03-22 22:11:12 -0700

    Initial commit

---
+++ final/ArtInfo.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows;
+
+namespace final
+{
+     class ArtInfo
+    {
+        string _name;
+        string _date;
+        string Artist;
+        string _ArtStyle;
+
+        public ArtInfo(string name, string date, string artist, string artStyle)
+        {
+            _name = name;
+            _date = date;
+            Artist = artist;
+            _ArtStyle = artStyle;
+        }
+
+        public string Name { get => _name; set => _name = value; }
+        public string Date { get => _date; set => _date = value; }
+        public string Artist0 { get => Artist; set => Artist = value; }
+        public string ArtStyle { get => _ArtStyle; set => _ArtStyle = value; }
+       
+    }
+  
+}
---
+++ final/MainWindow.xaml.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Xml.Linq;
+
+namespace final
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ObservableCollection<ArtInfo> artInfos = new ObservableCollection<ArtInfo>();
+        string filePath = "";
+        string selectedArtStyle = "";
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            ObservableCollection<ArtInfo> artInfos = new ObservableCollection<ArtInfo>();
+            DateTime date = new DateTime();
+            txtDisplay.Text = txtDate.ToString();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog files = new OpenFileDialog();
+
+            if (files.ShowDialog() == true)
+            {
+                filePath = files.FileName;
+
+                lblfilePath.Content = filePath;
+
+                BitmapImage image = new BitmapImage();
+                Uri uri = new Uri(filePath);
+
+                image.BeginInit();
+                image.UriSource = uri;
+                image.EndInit();
+
+                imgDisplay.Source = image;
+            }
+        }
+
+        private void rb_Checked(object sender, RoutedEventArgs e)
+        {
+            RadioButton radioButton = sender as RadioButton;
+            if (radioButton != null && radioButton.IsChecked == true)
+            {
+                selectedArtStyle = radioButton.Content.ToString();
+            }
+        }
+
+        private void btnSumit_Click(object sender, RoutedEventArgs e)
+        {
+            ArtInfo artInfo = new ArtInfo(txtname.Text, txtDate.Text, txtArtist.Text, selectedArtStyle);
+
+            artInfos.Add(artInfo);
+            listView.ItemsSource = artInfos;
+
+            txtDisplay.Text = $"{txtDate.Text}\n{txtname.Text}\n{txtArtist.Text}\n{txtArtInformartion.Text} ";
+          //  listView.Items.Add(artInfo);
+
+            RadioButton selectedRadioButton = stackPanel.Children.OfType<RadioButton>().FirstOrDefault(r => r.IsChecked == true);
+            if (selectedRadioButton != null)
+            {
+                artInfo.ArtStyle = selectedRadioButton.Content.ToString();
+            }
+        
+
+
+
+        }
+    }
+
+}
